93feaff4e8533d22ab2a802f0b7e3c9bf19a611a
Blake Yeboah <blakekyeboah@gmail.com>
2021-07-06 19:07:12 +1000

    Created Board Container component

---
+++ src/components/BoardContainer.js
@@ -0,0 +1,12 @@
+// Components
+import Board from "./Board";
+
+const BoardContainer = () => {
+   return (
+      <div className="board-container">
+         <Board />
+      </div>
+   );
+};
+
+export default BoardContainer;
