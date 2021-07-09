4bb1e64cb184ccac1df72eabe92377907e744888
Blake Yeboah <blakekyeboah@gmail.com>
2021-07-09 19:25:42 +1000

    Started Creating ScoreBox Component

---
+++ src/components/ScoreBox.js
@@ -0,0 +1,11 @@
+const ScoreBox = () => {
+   return (
+      <div className="score-box">
+         <div className="player-container white">
+            <h1 className="player-title">White</h1>
+         </div>
+      </div>
+   );
+};
+
+export default ScoreBox;
