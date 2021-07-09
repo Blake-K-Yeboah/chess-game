6c7829ef523b5265d5acc6dcf5cdce6eb31298f0
Blake Yeboah <blakekyeboah@gmail.com>
2021-07-09 20:53:32 +1000

    Finished ScoreBox Component

--- src/components/ScoreBox.js
+++ src/components/ScoreBox.js
@@ -1,8 +1,47 @@
-const ScoreBox = () => {
+const ScoreBox = ({ collectedPieces }) => {
+   const displayPiece = (piece) => {
+      switch (piece) {
+         case "king":
+            return <span>&#9812;</span>;
+         case "queen":
+            return <span>&#9813;</span>;
+         case "pawn":
+            return <span>&#9817;</span>;
+         case "bishop":
+            return <span>&#9815;</span>;
+         case "knight":
+            return <span>&#9816;</span>;
+         case "rook":
+            return <span>&#9814;</span>;
+         default:
+            console.log("this shouldnt happen.");
+      }
+   };
+
    return (
       <div className="score-box">
          <div className="player-container white">
-            <h1 className="player-title">White</h1>
+            <span className="player-icon">&#9812;</span>
+            <div className="content">
+               <h3 className="player-title">White</h3>
+               <div className="pieces">
+                  {collectedPieces.light.map((piece) => {
+                     return displayPiece(piece);
+                  })}
+               </div>
+            </div>
+         </div>
+
+         <div className="player-container black">
+            <span className="player-icon">&#9812;</span>
+            <div className="content">
+               <h3 className="player-title">Black</h3>
+               <div className="pieces">
+                  {collectedPieces.dark.map((piece) => {
+                     return displayPiece(piece);
+                  })}
+               </div>
+            </div>
          </div>
       </div>
    );
