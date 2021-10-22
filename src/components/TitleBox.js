b1dfd8ffe0dcff39da1fe83cd0c800eaeb6b9681
Blake Yeboah <blakekyeboah@gmail.com>
2021-10-22 22:07:14 +1000

    Displays whos turn it is

--- src/components/TitleBox.js
+++ src/components/TitleBox.js
@@ -1,12 +1,16 @@
 import Logo from "../img/Chess Logo.jpg";
 
-const TitleBox = () => {
-   return (
-      <div className="title-box">
-         <img src={Logo} alt="Chess Logo" className="logo" />
-         <h1 className="title">Chess</h1>
-      </div>
-   );
+const TitleBox = ({ turn }) => {
+    return (
+        <div className="title-box">
+            <img src={Logo} alt="Chess Logo" className="logo" />
+            <h1 className="title">Chess</h1>
+            <div className={`${turn} turn`}>
+                Turn <br />
+                <span className="player-icon">&#9812;</span>
+            </div>
+        </div>
+    );
 };
 
 export default TitleBox;
